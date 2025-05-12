bf1ddd5b353cdd352d3dcaa88eb2611559e84f99
asm-dev <anasalamancamontero@gmail.com>
2025-05-12 17:40:32 +0100

    Init form module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [{ path: 'table', loadChildren: () => import('./modules/table/table.module').then(m => m.TableModule) }];
+const routes: Routes = [{ path: 'table', loadChildren: () => import('./modules/table/table.module').then(m => m.TableModule) }, { path: 'form', loadChildren: () => import('./modules/form/form.module').then(m => m.FormModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
