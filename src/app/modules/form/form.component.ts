449281134f5d698a3d1f3ecc0880e70b6e32d3c3
asm-dev <anasalamancamontero@gmail.com>
2025-05-12 17:51:59 +0100

    Add form builder and validators

--- src/app/modules/form/form.component.ts
+++ src/app/modules/form/form.component.ts
@@ -1,11 +1,30 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-form',
   standalone: false,
   templateUrl: './form.component.html',
-  styleUrl: './form.component.scss'
+  styleUrl: './form.component.scss',
 })
 export class FormComponent {
+  userForm: FormGroup;
 
+  constructor(private fb: FormBuilder) {
+    this.userForm = this.fb.group({
+      name: ['', Validators.required],
+      lastName: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      phone: ['', [Validators.required, Validators.pattern(/^[0-9]{9}$/)]],
+      age: ['', [Validators.required, Validators.min(18)]],
+      address: ['', Validators.required],
+    });
+  }
+
+  submitForm(): void {
+    if (this.userForm.valid) {
+      console.log(this.userForm.value);
+    }
+  }
 }
--- src/app/modules/form/form.module.ts
+++ src/app/modules/form/form.module.ts
@@ -1,17 +1,11 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { FormRoutingModule } from './form-routing.module';
 import { FormComponent } from './form.component';
 
-
 @NgModule({
-  declarations: [
-    FormComponent
-  ],
-  imports: [
-    CommonModule,
-    FormRoutingModule
-  ]
+  declarations: [FormComponent],
+  imports: [CommonModule, FormRoutingModule, ReactiveFormsModule],
 })
-export class FormModule { }
+export class FormModule {}
